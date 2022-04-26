5d732b3a13420d70653dfa3625350ebdaea873e6
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2022-04-26 21:55:08 +0900

    map

--- chapter12/game.py
+++ chapter12/game.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 WIDTH = 640
 HEIGHT = 480
@@ -9,30 +10,43 @@ map_dic = {
     2: "tile2",  # 道路
 }
 
+map_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+
 class Map:
-    def __init__(self, map_file, map_dic):
-
-        self.map_data = np.zeros((8, 10))
+    def __init__(self, map_dic, map_data, tile_size):
         self.map_dic = map_dic
-
-        with open(map_file) as file:
-            for i, line in enumerate(file):
-                for j, number in enumerate(line.strip()):
-                    print(f"{i} {j}")
-                    self.map_data[i][j] = number
+        self.map_data = map_data
+        self.tile_size = tile_size
 
     def draw(self):
+        max_i = WIDTH // self.tile_size
+        max_j = HEIGHT // self.tile_size
 
-        for i in range(8):
-            for j in range(10):
-                x = 64 * j
-                y = 64 * i
-                number = self.map_data[i][j]
-                image = self.map_dic[number]
-                screen.blit(image, (x, y))
+        for i, j in itertools.product(range(max_i), range(max_j)):
+            number = self.map_data[j][i]
+            image = self.map_dic[number]
+            screen.blit(image, (i * self.tile_size, j * self.tile_size))
 
 
-map = Map("map.txt", map_dic)
+map = Map(map_dic, map_data, 32)
+
 
 def draw():
     screen.fill("white")
